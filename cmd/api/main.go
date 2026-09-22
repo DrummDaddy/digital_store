8c95d6b1af03b9c295a9a4bbbc698847c2bd8317
DrummDaddy <alexpospelov87@gmail.com>
2026-09-22 19:47:24 +0700

    update main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,6 +12,8 @@ import (
 
 	"github.com/DrummDaddy/digital_store/internal/config"
 	"github.com/DrummDaddy/digital_store/internal/database"
+	"github.com/DrummDaddy/digital_store/internal/httpapi"
+	"github.com/DrummDaddy/digital_store/internal/order"
 )
 
 func main() {
@@ -22,34 +25,73 @@ func main() {
 			},
 		),
 	)
+
 	cfg := config.Load()
 
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
 	defer stop()
 
 	db, err := database.Open(ctx, cfg.DatabaseURL)
 	if err != nil {
 		logger.Error(
-			"databaase connection failed",
-			"error",
-			err,
+			"database connection failed",
+			"error", err,
 		)
 		os.Exit(1)
 	}
 	defer db.Close()
-	logger.Info(
-		"database connection succeeded",
-		"address",
-		cfg.HttpAddr,
-	)
-	<-ctx.Done()
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	_ = shutdownCtx.Ctx
 
-	logger.Info("application sopped")
+	orderRepo := order.NewRepository(db)
 
-	if errors.Is(ctx.Err(), context.Canceled) {
-		return
+	api := httpapi.NewServer(orderRepo, logger)
+
+	server := &http.Server{
+		Addr:              cfg.HTTPAddr,
+		Handler:           api.Router(),
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
 	}
+
+	serverErr := make(chan error, 1)
+
+	go func() {
+		logger.Info(
+			"HTTP server started",
+			"address", cfg.HTTPAddr,
+		)
+
+		serverErr <- server.ListenAndServe()
+	}()
+
+	select {
+	case err := <-serverErr:
+		if !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("HTTP server failed", "error", err)
+			os.Exit(1)
+		}
+
+	case <-ctx.Done():
+		logger.Info("shutdown signal received")
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		logger.Error(
+			"HTTP server shutdown failed",
+			"error", err,
+		)
+	}
+
+	logger.Info("application stopped")
 }
